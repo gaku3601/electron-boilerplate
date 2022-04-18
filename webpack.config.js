fa6ccea229f96c1cf14ceccd748430074127d69d
gaku <pro.gaku@gmail.com>
2022-04-19 04:48:25 +0900

    format

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
-const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: path.join(__dirname, 'src/index.tsx'),
-    devServer: {
-        open: true, // 自動でブラウザで開く
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        port: 3000,
+  mode: 'development',
+  entry: path.join(__dirname, 'src/index.tsx'),
+  devServer: {
+    open: true, // 自動でブラウザで開く
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+    port: 3000,
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'node_modules')],
+    extensions: ['.js', '.ts', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-    resolve: {
-        modules: [path.resolve(__dirname, "node_modules")],
-        extensions: [".js", ".ts", ".tsx"],
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
-    },
-    module:  {
-        rules: [
-            {
-                test: /\.(tsx|ts)$/,
-                exclude: /(node_modules)/,
-                use: [
-                    {loader: 'ts-loader'}
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "public/index.html"),
-        }),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        exclude: /(node_modules)/,
+        use: [
+          { loader: 'ts-loader' },
+        ],
+      },
     ],
-};
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+}
